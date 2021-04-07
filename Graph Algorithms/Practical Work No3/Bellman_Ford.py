bf6c685a7902fa0b9014894f54f4d409433f546f
912-Danicico-George <georgedanicico@yahoo.com>
2021-04-07 23:32:45 +0300

    update

--- Graph Algorithms/Practical Work No3/Bellman_Ford.py
+++ Graph Algorithms/Practical Work No3/Bellman_Ford.py
@@ -1,16 +1,26 @@
 
 # the function below generates the path from source vertex to destination vertex.
-def DFS(graph, src, vertex, path, dist):
+# def DFS(graph, src, vertex, path, dist):
+#
+#     path.append(vertex)
+#     if vertex != src:
+#         for neighbour in graph.getInboundNeighbours(vertex):
+#             if dist[neighbour] == dist[vertex] - graph.getCostOfEdge((neighbour, vertex)):
+#                 return DFS(graph, src, neighbour, path[:], dist)
+#
+#     else:
+#         return path
 
-    path.append(vertex)
-    if vertex != src:
-        for neighbour in graph.getInboundNeighbours(vertex):
-            if dist[neighbour] == dist[vertex] - graph.getCostOfEdge((neighbour, vertex)):
-                return DFS(graph, src, neighbour, path[:], dist)
+def generatePath(prev, src, destination):
+    path = [destination]
+    vertex = destination
+    while vertex != src:
+        path.append(prev[vertex])
+        vertex = prev[vertex]
 
-    else:
-        return path
+    path.reverse()
 
+    return path
 
 def BellmanFordAlgortihm(graph, v1, v2):
     """
@@ -26,7 +36,7 @@ def BellmanFordAlgortihm(graph, v1, v2):
     dist = [float("inf")] * graph.NrOfVertices
     # Except for the source vertex
     dist[v1] = 0
-
+    prev = [0] * graph.NrOfVertices
     changed = True
     while changed:
         changed = False
@@ -36,6 +46,7 @@ def BellmanFordAlgortihm(graph, v1, v2):
             cost = graph.getCostOfEdge(edge)
             if dist[vertex1] != float("inf") and dist[vertex2] > dist[vertex1] + cost:
                 dist[vertex2] = dist[vertex1] + cost
+                prev[vertex2] = vertex1
                 changed = True
 
     for edge in graph.getAllEdges():
@@ -46,9 +57,8 @@ def BellmanFordAlgortihm(graph, v1, v2):
             raise Exception("Negative cost cycle detected!\n")
 
     if dist[v2] != float("inf"):
-        path = DFS(graph, v1, v2, [], dist)
-        path.reverse()
 
+        path = generatePath(prev, v1, v2)
         return dist[v2], path
 
     return None
